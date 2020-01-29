02af89a133ed043bc28e1fcbfae53dadf66a8eb2
Eredost <michael.coutin62@gmail.com>
2020-01-29 16:37:54 +0100

    Integration of home section done and added javascript events linked to the header

--- app/js/app.js
+++ app/js/app.js
@@ -4,8 +4,12 @@ var app = {
   init: function() {
     console.log('init');
 
+    // Handle click event on header burger button
     $('.main-nav__button').on('click', app.handleNavOpening);
     $('.menu__closing').on('click', app.handleNavClosing);
+
+    // Handle header menu position when leaving introduction section
+    app.handleNavPosition();
   },
 
   handleNavOpening: function () {
@@ -14,6 +18,17 @@ var app = {
 
   handleNavClosing: function () {
     $('body').removeClass('menu-visible');
+  },
+
+  handleNavPosition: function () {
+    $('#home').scrollex({
+      enter: function () {
+        $('.header').removeClass('detached');
+      },
+      leave: function () {
+        $('.header').addClass('detached');
+      }
+    });
   }
 };
 
