2b27ee569657630807a7ffca8edac6c6dce31775
Eredost <michael.coutin62@gmail.com>
2020-01-29 15:15:04 +0100

    Functional burger menu and start of integration of the introduction section

--- app/js/app.js
+++ app/js/app.js
@@ -3,6 +3,17 @@ require('jquery.scrollex');
 var app = {
   init: function() {
     console.log('init');
+
+    $('.main-nav__button').on('click', app.handleNavOpening);
+    $('.menu__closing').on('click', app.handleNavClosing);
+  },
+
+  handleNavOpening: function () {
+    $('body').addClass('menu-visible');
+  },
+
+  handleNavClosing: function () {
+    $('body').removeClass('menu-visible');
   }
 };
 
